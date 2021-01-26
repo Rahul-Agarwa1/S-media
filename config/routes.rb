4de23212b8a452888ec99284ba6857680e23482a
Rahul Agarwal <Rahul@Admins-MacBook-Pro.local>
2021-01-26 23:00:20 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new' 
+  end
+
+  root 'welcome#index'
+
+  
+
+end
