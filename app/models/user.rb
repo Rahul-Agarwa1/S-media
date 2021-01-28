e9dea9bc0e393a3abeac405a65fa1a6144709276
Rahul Agarwal <Rahul@Admins-MacBook-Pro.local>
2021-01-28 15:37:49 +0530

    add Image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  mount_uploader :avatar, AvatarUploader
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   devise :database_authenticatable, :registerable,
