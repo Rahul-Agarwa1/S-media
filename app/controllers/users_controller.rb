86b0aab08f163ebb4e3d8d624fcb863e0652f415
Rahul Agarwal <Rahul@Admins-MacBook-Pro.local>
2021-01-29 18:38:30 +0530

    Search friends using Ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,45 +9,29 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend] 
     
-    # if params[:friend].present?
-    #   @friends = User.search(params[:friend])
-    #   @friends = current_user.except_current_user(@friends)
-    #   if @friends
-    #     respond_to do |format|
-    #       format.js { render partial: 'users/friend_result' }
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       flash.now[:alert] = "Couldn't find user"
-    #       format.js { render partial: 'users/friend_result' }
-    #     end
-    #   end    
-    # else
-    #   respond_to do |format|
-    #     flash.now[:alert] = "Please enter a friend name or email to search"
-    #     format.js { render partial: 'users/friend_result' }
-    #   end
-    # end
+    if params[:friend].present?
+      @friends = User.all
+      # @friends = User.search(params[:friend])
+      # byebug
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 
 
 end
 
-
-#  <div class='search-area'>
-#   <h3>Search Friends</h3>
-#   <%= form_tag search_friend_path, remote: true, method: :get do %>
-#     <div class="form-group row">
-#       <div class="col-sm-9 no-right-padding">
-#         <%= text_field_tag :friend, params[:friend], placeholder: "Name or email", autofocus: true, class: "form-control form-control-lg" %>
-#       </div>
-#       <div class="col-sm-3 no-left-padding">
-#         <%= button_tag type: :submit, class: "btn btn-success" do %>
-#           <%= fa_icon 'search 2x' %>
-#         <% end %>
-#       </div>
-#     </div>
-#   <% end %>
-# </div>
